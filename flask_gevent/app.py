1b540ae54f43e18f14e43ab2d85de100d1847831
RobertH1235 <robert.hartles@ontoforce.com>
2023-07-04 12:37:17 +0100

    first commit

---
+++ flask_gevent/app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, request, send_from_directory
+from gevent.pywsgi import WSGIServer
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part in the request"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No file selected"
+
+    if file and allowed_file(file.filename):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return "File uploaded successfully"
+    else:
+        return "Invalid file"
+
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+def create_app():
+    return app
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
---
+++ uploads_downloads/download.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Number of concurrent downloads
+num_downloads=10
+
+# Filename to download
+filename="1GB.png"
+
+# Download URL
+download_url="http://192.168.1.118:5002/download/$filename"
+
+# Folder to save the downloaded files
+download_folder="./download_folder"
+
+# Folder to save the timing information
+timing_folder="./timing_folder"
+
+# Create the download and timing folders if they don't exist
+mkdir -p "$download_folder"
+mkdir -p "$timing_folder"
+
+# Function to download a file and measure the time
+download_file() {
+  start_time=$(date +%s.%N)
+  curl -o "$download_folder/$filename.$1" "$download_url"
+  end_time=$(date +%s.%N)
+  elapsed_time=$(bc <<< "$end_time - $start_time")
+  echo "File $1: Started at $(date -d @$start_time +'%H:%M:%S'), Finished at $(date -d @$end_time +'%H:%M:%S'), Elapsed time: $elapsed_time seconds" >> "$timing_folder/timing_$(date +'%Y%m%d_%H%M%S').txt"
+}
+
+# Start the loop
+for ((i=1; i<=$num_downloads; i++))
+do
+  # Download the file in the background and measure the time
+  download_file $i &
+done
+
+# Wait for all background processes to finish
+wait
